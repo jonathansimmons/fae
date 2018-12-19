66f5835c1d70bdc9eb93dd8b04b436ea2f82a572
Jeremy Clemans <jeremy@wearefine.com>
2018-12-19 14:38:47 -0800

    77615 fixes for errors on activity log page and its associated filtering logic

--- app/models/fae/change.rb
+++ app/models/fae/change.rb
@@ -8,6 +8,28 @@ module Fae
 
     serialize :updated_attributes
 
+    def change_item_link
+      text = "#{self.changeable_type.gsub('Fae::','')}: "
+      test_source_method = :data_source_exists?
+
+      if self.changeable_type.exclude?('Fae') && self.changeable_type.exclude?('Page') && !ActiveRecord::Base.connection.send(test_source_method, self.changeable_type.tableize)
+        return "#{self.changeable_type}: model destroyed"
+      else
+        display_text = self.try(:changeable).try(:fae_display_field)
+        display_text = ([Integer, Symbol].include? display_text.class) ? display_text.to_s : display_text
+        text += display_text || "##{self.changeable_id}"
+
+        begin
+          return link_to text, fae.edit_content_block_path(self.changeable.slug) if self.changeable_type == 'Fae::StaticPage'
+          parent = self.changeable.respond_to?(:fae_parent) ? self.changeable.fae_parent : nil
+          edit_path = edit_polymorphic_path([main_app, fae_scope, parent, self.changeable])
+          return link_to text, edit_path
+        rescue
+          return text
+        end
+      end
+    end
+
     class << self
       # writing current_user to thread for thread safety
       def current_user=(user)
@@ -30,10 +52,10 @@ module Fae
         conditions[:change_type] = params['type'] if params['type'].present?
 
         date_scope = case params['date']
-          when 'Last Hour' then   ['fae_changes.updated_at >= ?', 60.minutes.ago]
-          when 'Last Day' then    ['fae_changes.updated_at >= ?', 1.day.ago]
-          when 'Last Week' then   ['fae_changes.updated_at >= ?', 1.week.ago]
-          when 'Last Month' then  ['fae_changes.updated_at >= ?', 1.month.ago]
+          when URI.escape('Last Hour') then   ['fae_changes.updated_at >= ?', 60.minutes.ago]
+          when URI.escape('Last Day' ) then   ['fae_changes.updated_at >= ?', 1.day.ago]
+          when URI.escape('Last Week') then   ['fae_changes.updated_at >= ?', 1.week.ago]
+          when URI.escape('Last Month') then  ['fae_changes.updated_at >= ?', 1.month.ago]
         end
 
         # use good 'ol MySQL to search if search param is present
@@ -45,7 +67,7 @@ module Fae
         # apply conditions and search from above to our scope
         order(id: :desc)
           .includes(:user).references(:user)
-          .where(Arel.sql(date_scope)).where(conditions).where(Arel.sql(search))
+          .where(date_scope).where(conditions).where(search)
       end
 
     end
